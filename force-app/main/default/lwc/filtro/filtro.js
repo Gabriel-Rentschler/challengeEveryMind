4467110bfed8b7e77907d583009d3f1d8a22369d
Gabriel-Rentschler <madiness444555666@gmail.com.br>
2023-08-21 13:17:17 -0300

    filtro completo com api do GPT

--- force-app/main/default/lwc/filtro/filtro.js
+++ force-app/main/default/lwc/filtro/filtro.js
@@ -19,10 +19,11 @@ export default class Filtro extends LightningElement {
     }
 
     sendQuestion() {
-        var searchInput = "Analise os currículos e responda apenas com o nome do(s) candidato(s) mais adequado(s):"
+        var searchInput = "Analise os currículos e responda apenas com o nome dos candidatos mais adequados:"
         searchInput += this.curriculums();
         searchInput += this.template.querySelector('input[name="search"]').value;
-        const OPENAI_API_KEY = "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";
+
+        const OPENAI_API_KEY = "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";
 
         const candidatos = {};
         candidatos["matheus"] = this.template.querySelector('div[class="matheus"]');
@@ -50,12 +51,12 @@ export default class Filtro extends LightningElement {
                 console.log(`Error: ${json.error.message}`);
             } else if (json.choices?.[0].message) {
                 var text = json.choices[0].message.content || "Sem resposta";
-                const responseSplit = text.split("Currículo ");
+                const responseSplit = text.replaceAll("e ", ',').split(",");
                 console.log(text);
                 console.log(responseSplit[1]);
                 
                 for (var key in candidatos) {
-                    if (key == responseSplit[1].toLowerCase())
+                    if (key != text.toLowerCase())
                         candidatos[key].style.display = 'none';
                 }
 
@@ -69,8 +70,10 @@ export default class Filtro extends LightningElement {
     }
 
     curriculums() {
-        var curriculums = "Currículo Matheus:  \n Desenvolvedor de Aplicações \n Experiência como Desenvolvedor e Quality Assurance, atuando em projetos para empresas na área de telecomunicações e petrolífera. Aprimorando minhas habilidades para atender as necessidades do cliente e auxiliar a equipe, utilizando metodologias ágeis. Com sólida experiência internacional e Inglês fluente. Especialidade em Salesforce, Java e Cloud. \n";
-        curriculums += "Currículo Cahue: \n Desenvolvedor de Aplicações \n Minha exposição internacional em uma idade jovem me deu uma profunda experiência cobrindo vários aspectos da vida profissional, como atendimento ao cliente, vendas e estratégia. \n De volta ao Brasil desde 2019, iniciei meus estudos e carreira na Tecnologia da Informação. Atualmente atuando como iOS Developer Trainee, utilizando a linguagem de programação Swift (Storyboard, UIKit, Auto Layout, ViewCode e MVC Design Pattern) e a IDE XCode. \n";
+        var curriculums = "Currículo Matheus:  \n Desenvolvedor de Aplicações \n Experiência como Desenvolvedor e Quality Assurance há 4 anos, atuando em projetos para empresas na área de telecomunicações e petrolífera. Aprimorando minhas habilidades para atender as necessidades do cliente e auxiliar a equipe, utilizando metodologias ágeis. Com sólida experiência internacional e Inglês fluente. Especialidade em Salesforce, Java e Cloud. \n";
+        curriculums += "Currículo Cahue: \n Desenvolvedor de Aplicações \n Minha exposição internacional em uma idade jovem me deu uma profunda experiência cobrindo vários aspectos da vida profissional, como atendimento ao cliente, vendas e estratégia. De volta ao Brasil desde 2019, iniciei meus estudos e carreira na Tecnologia da Informação. Atualmente atuando como iOS Developer Trainee, utilizando a linguagem de programação Swift (Storyboard, UIKit, Auto Layout, ViewCode e MVC Design Pattern) e a IDE XCode com 2 anos de experiência. \n";
+        curriculums += "Currículo Gabriel: \n Desenvolvedor de Aplicações \n Experiência como Desenvolvedor e Quality Assurance, atuando em projetos para empresas na área de jogos, com 3 anos de experiência. Aprimorando minhas habilidades para atender as necessidades do cliente e auxiliar a equipe, utilizando metodologias ágeis. Com sólida experiência internacional e Inglês fluente. Especialidade em C#, Java e Cloud. \n"
+        curriculums += "Currículo Gustavo: \n Analista de banco de dados \n Experiência como DBA, atuando em projetos para empresas na área de telecomunicações e petrolífera por 5 anos. Conhecimentos avançados em SQL e banco de dados Oracle, além de data warehousing."
         return curriculums;
     }
 }
