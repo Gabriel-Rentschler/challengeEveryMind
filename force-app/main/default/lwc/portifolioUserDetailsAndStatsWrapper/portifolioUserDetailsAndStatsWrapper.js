d98e1ed904af1a51ca1d0d35af6f494410b5bb82
Gabriel-Rentschler <madiness444555666@gmail.com.br>
2023-08-21 13:32:43 -0300

    remoção de resíduos

--- force-app/main/default/lwc/portifolioUserDetailsAndStatsWrapper/portifolioUserDetailsAndStatsWrapper.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class PortifolioUserDetailsAndStatsWrapper extends LightningElement {
-    recordId="a01Hu00000x9SiYIAU"
-    objectApiName="Portfolio__c"
-}
